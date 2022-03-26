e6254d0a409a8710b40808880ba5d8bf2d2d7b25
Stone Comstock <comstockstone@yahoo.com>
2022-03-25 17:33:38 -0700

    Uppdated models and added index.js to models

---
+++ models/index.js
@@ -0,0 +1,23 @@
+// import models
+const Comment = require('./Comment');
+const Event = require('./Event');
+const User = require('./User');
+const Rsvp = require('./RSVP');
+
+Comment.belongsTo(User);
+Comment.belongsTo(Event);
+User.belongsToMany(Event, {
+  through: Rsvp,
+  foreignKey: 'userId',
+});
+Event.belongsToMany(User, {
+  through: Rsvp,
+  foreignKey: 'eventId',
+});
+
+module.exports = {
+  Comment,
+  Event,
+  User,
+  Rsvp,
+};
