357fee2d4b4ae4423239113c2b1a92c87c5a28ef
Stone Comstock <comstockstone@yahoo.com>
2022-03-26 14:21:15 -0700

    Crud routing

--- models/Event.js
+++ models/Event.js
@@ -19,6 +19,10 @@ Event.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     startTime: {
       type: DataTypes.TIME,
       allowNull: false,
