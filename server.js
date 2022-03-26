4948d110942ba2300c5b6a48d0787b987f8783af
Mike Sanders <msanders331@gmail.com>
2022-03-26 11:07:38 -0700

    fixed server errors. Now running. Creating heroku and JawsDB link.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
@@ -9,7 +8,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -18,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
