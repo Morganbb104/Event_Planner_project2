830a65f05e81c139451e3f4e2b73e23361fb0c51
CHENG CHIEN <hcc780921@gmail.com>
2022-03-29 19:05:33 -0700

    homePage+userPage

--- public/js/script.js
+++ public/js/script.js
@@ -38,3 +38,64 @@ var swiper = new Swiper(".home-slider", {
         disableOnInteraction:false,
     }
   });
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// function choose event to user page
+
+  // function filter
+//filter time
+// filter event category
+//function state code
+// date,time picker(type input) for our date range 
+// filter will be a form, frontain //html input 
+// eventlistener for submist btn, put information on, and take 
+
+
+
+function submitFunction(){
+  document.getElementById('form').submit();
+}
+// submitFunction()
+
+
+
+// function filterSubmit(event){
+//   submit.textContent = "Chosen Events submmited!";
+//   event.preventDefault();
+// }
+// const form = document.getElementById('form').value;
+// const submit = document.getElementById('submit').value;
+
+// form.addEventListener('submit',filterSubmit(event){
+//   event.preventDefault();
+//   console.log(form)
+//   console.log(submit)
+  
+  
+// })
