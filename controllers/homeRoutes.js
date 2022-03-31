4c0e7475626b6a000f13f6769a71e047c9714e01
Namees Albayati <nameesalbayati@Nameess-MacBook-Pro.local>
2022-03-31 00:32:24 -0500

    add event filter function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,53 @@
 const routes=require('express').Router();
-const path=require('path')
+const {Event,Rsvp,User,Comment}=require('../models')
+const path=require('path');
+const router = require('./api/user-routes');
+const db=require('../config/connection');
+const { type } = require('express/lib/response');
+const { sequelize } = require('../models/Comment');
 routes.get('/signin',(req,res)=>{
     res.sendFile(path.join(__dirname,'../views/login.html'))
 })
 routes.get('/signup',(req,res)=>{
     res.sendFile(path.join(__dirname,'../views/signup.html'))
 })
+routes.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/homePage.html'))
+})
+
+
+router.post('/eventfilter',async(req,res)=>{
+try{
+        const categoryBody=req.body.event_category.toLowerCase();
+    console.log(categoryBody)
+
+const QueryData=`select * from Event where category="${categoryBody}" AND state="${req.body.state}";`;
+const result= await db.query(QueryData,{type:db.QueryTypes.SELECT});
+
+if(result==false){
+    console.log('user selection not found')
+
+    res.json({message:"Not Found here!!Please try again"})
+}else{
+    console.log(result);
+    // RendeData will be send it dynamically through handlebars
+
+
+
+
+}
+
+
+
+}catch(err){
+    res.json(err)
+}
+
+})
+
+
+
+
 module.exports=routes;
+
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,43 @@
 var swiper = new Swiper(".home-slider", {
-    effect: "coverflow",
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop:true,
+  autoplay:{
+    delay: 3000,
+    disableOnInteraction:false,
+  }
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
     grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2,
-      slideShadows: true,
-    },
     loop:true,
-    autoplay:{
-      delay: 3000,
-      disableOnInteraction:false,
-    }
-  });
-  
-  var swiper = new Swiper(".review-slider", {
-      slidesPerView: 1,
-      grabCursor: true,
-      loop:true,
-      spaceBetween: 10,
-      breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        700: {
-          slidesPerView: 2,
-        },
-        1050: {
-          slidesPerView: 3,
-        },    
+    spaceBetween: 10,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
       },
-      autoplay:{
-        delay: 5000,
-        disableOnInteraction:false,
-    }
-  });
+      700: {
+        slidesPerView: 2,
+      },
+      1050: {
+        slidesPerView: 3,
+      },    
+    },
+    autoplay:{
+      delay: 5000,
+      disableOnInteraction:false,
+  }
+});
 
 
 // Get the modal
@@ -51,24 +51,24 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+if (event.target == modal) {
+  modal.style.display = "none";
+}
 }
 
 // function choose event to user page
 
-  // function filter
+// function filter
 //filter time
 // filter event category
 //function state code
@@ -78,10 +78,34 @@ window.onclick = function(event) {
 
 
 
-function submitFunction(){
-  window.alert('test');
-  
-  // document.getElementById('form').submit();
+const form=document.getElementById('form')
+const states=document.getElementById('states');
+const event_category=document.getElementById('event_category');
+const startT=document.getElementById('startT');
+const endT=document.getElementById('endT');
+
+const submitFunction=async (e)=>{
+  alert('h')
+  e.preventDefault()
+const filterData={
+  state:states.value.trim(),
+  event_category:event_category.value.trim(),
+  startT:startT.value.trim(),
+  endT:endT.value.trim()
+
 }
 
+const fetchFillter=await fetch('/eventfilter',{
+  method:"POST",
+  headers: { 'content-type': 'application/json' },
+  body:JSON.stringify(filterData)
 
+})
+const result=await fetchFillter.json().then((data)=>{
+window.alert(data.message)
+})
+
+
+}
+
+form.addEventListener('submit',submitFunction)
