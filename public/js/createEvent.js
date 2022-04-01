33203165e18a17c98c1784632effe169a6866e9f
Mike Sanders <msanders331@gmail.com>
2022-03-31 20:48:37 -0700

    created the submit event form. Need to test

--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -3,8 +3,10 @@ const eventDescription = document.querySelector('#event-description');
 const eventState = document.querySelector('#event-state');
 const eventLocation = document.querySelector('#event-location');
 const eventCategory = document.querySelector('#event-category');
-const eventStart = document.querySelector('#start-date');
-const eventEnd = document.querySelector('#end-date');
+const eventStartDate = document.querySelector('#start-date');
+const eventEndDate = document.querySelector('#end-date');
+const eventStartTime = document.querySelector('#start-time');
+const eventEndTime = document.querySelector('#end-time');
 const submitButton = document.querySelector('#create-submit')
 
 //function for submission 
@@ -13,7 +15,24 @@ const createEvent = async (event) =>{
 
     const eventBody = {
         location: eventLocation.value.trim(),
+        state: eventState.value,
         category: eventCategory.value,
-        
-    }
+        startDate:eventStartDate.value,
+        endDate: eventEndDate.value,
+        startTime: eventStartTime.value,
+        endTime: eventEndTime.value,
+        eventDescription: eventDescription.value,
+    };
+
+    //sending event details to the server using POST request
+    const fetchNewEvent = await fetch('/events', {
+        method: 'POST',
+        headers:{
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(fetchNewEvent),
+    })
 }
+
+//on submit button being clicked then add the details.
+submitButton.addEventListener('submit', createEvent);
